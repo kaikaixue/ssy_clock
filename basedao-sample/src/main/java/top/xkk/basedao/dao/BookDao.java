c6ab891112f88aaa580173b8e25428f1d1aaa283
xuekaikai <2780739366@qq.com>
2021-11-15 00:11:25 +0800

    Day1 BaseDao封装 & Maven项目资源路径配置

---
+++ basedao-sample/src/main/java/top/xkk/basedao/dao/BookDao.java
@@ -0,0 +1,4 @@
+package top.xkk.basedao.dao;
+
+public interface BookDao<T> extends BaseDao<T> {
+}
---
+++ basedao-sample/src/main/java/top/xkk/basedao/dao/impl/BookDaoImpl.java
@@ -0,0 +1,7 @@
+package top.xkk.basedao.dao.impl;
+
+import top.xkk.basedao.dao.BookDao;
+import top.xkk.basedao.entity.Book;
+
+public class BookDaoImpl extends BaseDaoImpl<Book> implements BookDao<Book> {
+}
